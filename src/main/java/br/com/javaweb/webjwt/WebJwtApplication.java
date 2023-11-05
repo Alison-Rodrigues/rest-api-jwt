3fba2027d067a2b96213310d7cb5db759776c241
Alison Rodrigues <alison.si1302@gmail.com>
2023-11-05 00:24:23 -0300

    initial commit

---
+++ src/main/java/br/com/javaweb/webjwt/WebJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.javaweb.webjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javaweb/webjwt/WebJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaweb.webjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
